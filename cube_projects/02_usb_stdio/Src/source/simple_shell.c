7cb2de3719b37ea0d31bc3a6436f92b2870b0f97
Kamil Cukrowski <kamilcukrowski@gmail.com>
2017-09-10 01:20:02 +0200

    remove cube_projects

--- cube_projects/02_usb_stdio/Src/source/simple_shell.c
+++
@@ -1,7 +0,0 @@
-/*
- * simple_shell.c
- *
- *  Created on: 2 wrz 2017
- *      Author: Kamil Cukrowski <kamilcukrowski __at__ gmail.com>
- *     License: jointly under MIT License and the Beerware License.
- */
